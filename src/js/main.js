8cf90393ee1a33da5d1cae42a62fb54f9a0799f0
Eric Jaden Stautmeister <eric@stautmeister.ch>
2021-04-17 18:03:49 +0200

    init

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,5 @@
 const grid = document.querySelector('#hex-grid');
+const light = document.querySelector('.light')
 
 grid.addEventListener('mousemove', function(e) {
 	light.style.left = `${e.clientX}px`;
